eed0fca47f826b07de7fca213d856c859bd219c7
brunomorais <bruno.morais@gmail.com.br>
2023-03-30 19:28:55 -0300

    Add cores para o thema do projeto

---
+++ src/theme/color.ts
@@ -0,0 +1,7 @@
+export enum Colors {
+  bkgBody = "#43BF70",
+  logo = "#DB7307",
+  bkgButton = "#DB7000",
+  textButton = "#EDFAF2",
+  titleH1 = "#32A65C",
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,5 @@
+import { Colors } from "./color";
+
+export const theme = {
+  colors: Colors,
+};
