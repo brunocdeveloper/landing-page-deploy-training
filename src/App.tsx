6514f432d7903435180944511ab68b34eafc4617
brunomorais <bruno.morais@gmail.com.br>
2023-03-30 19:20:02 -0300

    Cria estrutura de pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <p>Testando se o deploy vai ser feito mesmo com outra branch</p>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
